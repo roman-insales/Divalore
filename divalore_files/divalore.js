44b6c872a2817658a7df8a56fdb4e149f154fc43
roman-insales <roman.insales@yandex.ru>
2020-03-16 20:05:14 +0300

    gogogo layouts

--- divalore_files/divalore.js
+++ divalore_files/divalore.js
@@ -1,3 +1,45 @@
+$(".Carousel").each(function() {
+
+    var $this = $(this),
+        $btns = $this.find(".Carousel-prev, .Carousel-next"),
+        $slider = $this.find(".Carousel-slider"),
+        $cards = $slider.find(">*"),
+        tot = $cards.length,
+        i = Math.floor(tot / 2); // Somewhere in the middle
+  
+    function anim() {
+      i = i < 0 ? tot - 1 : i % tot; // Fix index
+      var $active = $cards.eq(i);
+      $cards.removeClass('active prev next');
+      $active.addClass('active');
+      $active.prev().addClass('prev');
+      $active.next().addClass('next');
+      $slider.css({transform: `translateX(-${100*i}%)`}); // CSS! yey
+    }
+  
+    $cards.on("click", function() {
+      i = $cards.index(this);
+      anim();
+    });
+  
+    $btns.on("click", function() {
+      i = $(this).is(".Carousel-next") ? ++i : --i;
+      anim();
+    });
+  
+    anim();
+  
+    // Keyboard nav
+    $(document).on("keydown", function(e) {
+      var k = e.which;
+      if (k === 37 || k === 39) {
+        i = k === 39 ? ++i : --i;
+        anim();
+      }
+    });
+  
+  });
+
 // Select
 $('#language').each(function(){
     // Variables
